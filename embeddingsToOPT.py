eec514fec383934dae94a85dc6dc3eee55830462
Talia Seada <talias1235@gmail.com>
2024-01-07 12:52:27 +0200

    TODO: check the layer's changing

--- embeddingsToOPT.py
+++ embeddingsToOPT.py
@@ -1,8 +1,12 @@
+import torch
 from transformers import AutoTokenizer, OPTForCausalLM
 
 
+# TODO change this method - call the transformer
 def your_input_modification(hidden_states):
-    return hidden_states
+    # modified_states = hidden_states
+    modified_states = hidden_states * 0
+    return modified_states
 
 
 # Load the tokenizer and model
@@ -11,7 +15,8 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = OPTForCausalLM.from_pretrained(model_name)
 
 
-class CustomOPTLayer(model.base_model.decoder.layers[0].__class__):
+# second layer (?)
+class CustomOPTLayer(model.base_model.decoder.layers[3].__class__):
     def forward(self, hidden_states, attention_mask=None, layer_head_mask=None,
                 past_key_value=None, output_attentions=None, use_cache=None):
         # Modify hidden_states before passing them to the original forward method
@@ -19,14 +24,71 @@ class CustomOPTLayer(model.base_model.decoder.layers[0].__class__):
         return super().forward(modified_hidden_states, attention_mask, layer_head_mask,
                                past_key_value, output_attentions, use_cache)
 
+# # second layer (?)
+# class CustomOPTLayer2(model.base_model.decoder.layers[2].__class__):
+#     def forward(self, hidden_states, attention_mask=None, layer_head_mask=None,
+#                 past_key_value=None, output_attentions=None, use_cache=None):
+#         # Modify hidden_states before passing them to the original forward method
+#         modified_hidden_states = your_input_modification(hidden_states)
+#         return super().forward(modified_hidden_states, attention_mask, layer_head_mask,
+#                                past_key_value, output_attentions, use_cache)
 
-model.base_model.decoder.layers[0] = CustomOPTLayer(model.base_model.decoder.config)
+# changes a specific layer
+model.base_model.decoder.layers[3] = CustomOPTLayer(model.base_model.decoder.config)
+# # changes a specific layer
+# model.base_model.decoder.layers[2] = CustomOPTLayer2(model.base_model.decoder.config)
 
 # Encode the input text
-inputs = tokenizer("hello", return_tensors="pt")
+inputs = tokenizer("The capital of London is", return_tensors="pt")
 # Get model output including hidden states
-outputs = model(**inputs)
-print(outputs)
+outputs = model(**inputs, output_hidden_states=True)
+
+# Access the generated token IDs
+token_ids = outputs.logits.argmax(-1)
+# Decode the token IDs using the tokenizer
+generated_text = tokenizer.decode(token_ids[0], skip_special_tokens=True)
+# Print the generated text
+print("Generated Text: ", generated_text)
+
+# # OPT
+# words = ["לונדון היא עיר הבירה של", "חתול", "כלב", "אוטו", "ש", "היא"]
+# # file = "C:\\Users\\talia\\PycharmProjects\\TranslatorGPT\\output\\short_words_opt_1.csv"
+# # df = pd.read_csv(file)
+# # start = eval(df['start'][0])
+#
+# for word in words:
+#     custom_embeddings = hebrewLLM.pred(word)
+#     # Convert to PyTorch tensor
+#     custom_embeddings_tensor = torch.tensor(custom_embeddings, dtype=torch.float)
+#
+#     # Debugging: Print the hidden size of the model
+#     hidden_size = model.base_model.decoder.config.hidden_size
+#     print("Model's hidden size:", hidden_size)
+#     # Calculate sequence_length
+#     sequence_length = custom_embeddings_tensor.shape[1] // hidden_size
+#     if custom_embeddings_tensor.shape[1] % hidden_size != 0:
+#         print(
+#             f"Warning: The total number of features in the embeddings ({custom_embeddings_tensor.shape[1]}) is not a multiple of the model's hidden size ({hidden_size}).")
+#
+#     # Reshape embeddings
+#     custom_embeddings_tensor = custom_embeddings_tensor.view(1, sequence_length, hidden_size)
+#
+#     # Debugging: Print type and shape
+#     print("Type of custom_embeddings:", type(custom_embeddings_tensor))
+#     print("Shape of custom_embeddings:", custom_embeddings_tensor.shape)
+#
+#     # Forward pass
+#     output = model()
+#     print('word: ' + word)
+#     # get answer
+#     k = 2
+#     generated_ids = output.logits[:, -1, :].topk(k).indices
+#     # Decode the generated token IDs to text
+#     generated_text = tokenizer.decode(generated_ids.tolist()[0])
+#     print("Generated text:", generated_text)
+#
+#     print('\n<-------------------------------------------------------------------------------------->\n')
+
 
 # class CustomOPTForCausalLM(OPTForCausalLM):
 #     def __init__(self, config):
@@ -57,45 +119,3 @@ print(outputs)
 # generated_ids = output.logits[:, -1, :].topk(k).indices
 # generated_text = tokenizer.decode(generated_ids.tolist()[0])
 # print("Generated text:", generated_text)
-
-
-# OPT
-# words = ["לונדון היא עיר הבירה של","חתול", "כלב", "אוטו", "ש", "היא"]
-# # file = "C:\\Users\\talia\\PycharmProjects\\TranslatorGPT\\output\\short_words_opt_1.csv"
-# # df = pd.read_csv(file)
-# # start = eval(df['start'][0])
-#
-# for word in words:
-#     custom_embeddings = hebrewLLM.pred(word)
-#     # custom_embeddings = np.append(start, custom_embeddings, axis=0)
-#
-#     # Convert to PyTorch tensor
-#     custom_embeddings_tensor = torch.tensor(custom_embeddings, dtype=torch.float)
-#
-#     # Debugging: Print the hidden size of the model
-#     hidden_size = 512 #config.hidden_size
-#     print("Model's hidden size:", hidden_size)
-#     # Calculate sequence_length
-#     sequence_length = custom_embeddings_tensor.shape[1] // hidden_size
-#     if custom_embeddings_tensor.shape[1] % hidden_size != 0:
-#         print(
-#             f"Warning: The total number of features in the embeddings ({custom_embeddings_tensor.shape[1]}) is not a multiple of the model's hidden size ({hidden_size}).")
-#
-#     # Reshape embeddings
-#     custom_embeddings_tensor = custom_embeddings_tensor.view(1, sequence_length, hidden_size)
-#
-#     # Debugging: Print type and shape
-#     print("Type of custom_embeddings:", type(custom_embeddings_tensor))
-#     print("Shape of custom_embeddings:", custom_embeddings_tensor.shape)
-#
-#     # Forward pass
-#     output = custom_model(custom_embeddings=custom_embeddings_tensor)
-#     print('word: ' + word)
-#     # get answer
-#     k = 2
-#     generated_ids = output.logits[:, -1, :].topk(k).indices
-#     # Decode the generated token IDs to text
-#     generated_text = tokenizer.decode(generated_ids.tolist()[0])
-#     print("Generated text:", generated_text)
-#
-#     print('\n<-------------------------------------------------------------------------------------->\n')
